4ef83b4eb72bede178f5312d5550c66db360f3d3
Yuriy-St <yuriystaynov@gmail.com>
2023-08-28 01:34:17 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { Global } from '@emotion/react';
+import { GlobalStyles } from 'styles/GlobalStyles';
+import App from 'components/App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename='/goit-react-hw-05-movies'>
+    <React.StrictMode>
+      <Global styles={GlobalStyles} />
+      <App />
+    </React.StrictMode>
+  );
+  </BrowserRouter>
