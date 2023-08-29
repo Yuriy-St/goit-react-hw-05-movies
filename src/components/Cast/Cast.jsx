182915ed610553f8109e7829bde5b71d445e9847
Yuriy-St <yuriystaynov@gmail.com>
2023-08-29 16:42:29 +0300

    uncomented bugy cast component

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,24 +1,29 @@
+import Loader from 'components/Loader/Loader';
 import { useFetchMovieCredits } from 'hooks/useFetchMovieCredits';
 
 export default function Cast() {
   const { credits, isLoading, fetchError } = useFetchMovieCredits();
-  // const BASE_URL = 'http://image.tmdb.org/t/p/w300';
+  const BASE_URL = 'http://image.tmdb.org/t/p/w300';
   const { cast } = credits;
   console.log('Credits: ', credits);
   console.log('Cast: ', cast);
-  console.log('isLoading: ', isLoading); // just for linter
-  console.log('fetchError: ', fetchError); // just for linter
 
   return (
-    <div>Cast</div>
-    // <ul>
-    //   {cast.map(({ id, name, profile_path, character }) => {
-    //     <li key={id}>
-    //       <img src={`${BASE_URL}/${profile_path}`} alt={name} />
-    //       <p>{name}</p>
-    //       <p>Character: {character}</p>
-    //     </li>;
-    //   })}
-    // </ul>
+    // <div>Cast</div>
+    <>
+      {isLoading && <Loader />}
+      {cast?.length > 0 && (
+        <ul>
+          {cast.map(({ id, name, profile_path, character }) => (
+            <li key={id}>
+              <img src={`${BASE_URL}${profile_path}`} alt={name} />
+              <p>{name}</p>
+              <p>Character: {character}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+      {fetchError && <p>{fetchError}</p>}
+    </>
   );
 }
